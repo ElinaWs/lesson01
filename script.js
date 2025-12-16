b1c8c0a1caa6370b8fcf5797482b43fc510855cf
Elina <elina.alagozova@compasscollege.art>
2025-12-16 23:55:37 +0600

    add section 'find film', but it is a little bit broken

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addFilmsBn = document.getElementById('add-film-bt');
 const findFilmsBn = document.getElementById('search-film-bt');
 const markFilmsBn = document.getElementById('mark-film-bt');
 const contentBlock = document.getElementById('content');
+const appContent = document.getElementById('app-content');
 
 const movies = [
   { title: 'Inception', isAvailable: true },
@@ -25,7 +26,7 @@ const showFilms = () => {
     for(const movie of movies) {
         console.log(movie.title)
         const movieItem = document.createElement('li')
-        movieItem.textContent = `${movie.title} - ${movie.isAvailable ? 'Доступен' : 'Не доступен'}`
+        movieItem.textContent = `${movie.title} - ${movie.isAvailable ? 'Free' : 'Undefinded'}`
         moviesList.append(movieItem);
     }
 }
@@ -56,5 +57,45 @@ const addFilm = () => {
 
 
 addFilmsBn.addEventListener('click', addFilm)
+
+
+const findFilm = () => {
+    clearContent()
+
+    const input = document.createElement('input');
+    input.placeholder = "Text film's name";
+
+    const findFilmBtn = document.createElement('button');
+    findFilmBtn.textContent = 'Find';
+
+    findFilmBtn.onclick = () => {
+        clearApp();
+        const value = input.value.toLowerCase();
+
+        const foundMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(value)
+    );
+
+    if (foundMovies.length === 0) {
+      appContent.textContent = 'Film is not definded';
+      return;
+    }
+
+    const ul = document.createElement('ul');
+
+    foundMovies.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = `${movie.title} — ${movie.isAvailable ? 'Free' : 'Undefinded'}`;
+        ul.appendChild(li);
+    });
+
+    appContent.appendChild(ul);
+  };
+
+  appContent.append(input, findFilmBtn);
+}
+
+
+findFilmsBn.addEventListener('click', findFilm)
 
 
